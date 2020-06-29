e08525d151ae09495fd01a7ce6868eec47c41f6f
adbosa <adbosa@gmail.com>
2020-06-29 14:27:18 -0300

    Cleaning up repository.

--- matrix/imprime_matriz.py
+++
@@ -1,7 +0,0 @@
-def imprime_matriz(matriz):
-    '''(matriz) -> impressão visual de uma matriz
-    Organiza a visualização da matriz, deixando um linha
-    a baixo da outra.
-    '''
-    print(matriz[0][0])
-    return
