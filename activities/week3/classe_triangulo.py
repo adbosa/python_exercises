aa4f8fe9c18386f4dac82323cc56c2c43b0927b8
adbosa <adbosa@gmail.com>
2020-07-20 11:19:25 -0300

    Completion of week 3 main exercises.

--- activities/week3/classe_triangulo.py
+++
@@ -1,6 +0,0 @@
-''' Exercício 1: Uma classe para triângulos
-Defina a classe Triangulo cujo construtor recebe 3 valores inteiros
-correspondentes aos lados a, b e c de um triângulo. A classe triângulo
-também deve possuir um método perimetro, que não recebe parâmetros e
-devolve um valor inteiro correspondente ao perímetro do triângulo.
-'''
--- activities/week3/test_classe_triangulo.py
+++
@@ -1,5 +0,0 @@
-import pytest
-import classe_triangulo
-
-class TestaClasseTriangulo:
-    @pytest.te
