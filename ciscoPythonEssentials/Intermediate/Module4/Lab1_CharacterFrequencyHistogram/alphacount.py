a72e09367b2a92c3fb8b44a4f43bf5e277b328c1
Alex Bosa <adbosa@gmail.com>
2022-04-05 22:48:26 -0300

    Code refactoring to accept alphabetical or recurring ordering.

--- ciscoPythonEssentials/Intermediate/Module4/Lab1_CharacterFrequencyHistogram/alphacount.py
+++ ciscoPythonEssentials/Intermediate/Module4/Lab1_CharacterFrequencyHistogram/alphacount.py
@@ -1,25 +1,38 @@
 #!/usr/bin/python
 from os import strerror
 
-file = input('Enter the name of the file containing the text to parse: ')
-#file = 'test2'
-recurrence = {}
+def recurrence(filename):
+    recurrence = {}
+    try:
+        stream = open(filename, 'rt')
+        for line in stream:
+            for letter in line:
+                if letter.isalpha():
+                    if letter.lower() in recurrence:
+                        recurrence[letter.lower()] += 1
+                    else:
+                        recurrence[letter.lower()] = 1
+        stream.close()
+    except IOError as e:
+        print('I/O error occurred:', strerror(e.errno))
+    return recurrence
 
-try:
-    stream = open(file, 'rt')
-    for line in stream:
-        for letter in line:
-            if letter.isalpha():
-                if letter.lower() in recurrence:
-                    recurrence[letter.lower()] += 1
-                else:
-                    recurrence[letter.lower()] = 1
-    stream.close()
-except IOError as e:
-    print('I/O error occurred:', strerror(e.errno))
+def sort_alphabetically(dic):
+    return sorted(dic.items())
 
+def sort_by_recurrence(dic):
+    pass
+
+def build_histogram(dic, sorted_by='alpha'):
+    pass
+
+def write_output(histogram, filename):
+    pass
+
+
+"""
 for key, value in sorted(recurrence.items()):
-    print(key + '| ' + '===' * value) 
+print(key + '| ' + '===' * value) 
 
 highest = recurrence[max(recurrence, key=recurrence.get)]
 print('  ' + '-'*(highest*3))
@@ -31,4 +44,11 @@ for pos in range(highest):
     else:
         mark = str(pos+1)
     print(mark, end=' ')
-
+"""
+if __name__ == '__main__':
+    #file = input('Enter the name of the file containing the text to parse: ')
+    file = 'test2'
+    print('Result by alphabeticall order:')
+    print(sort_alphabetically(recurrence(file)))
+    print('Result by order of occurrence:')
+    print(sort_by_recurrence(recurrence(file)))
