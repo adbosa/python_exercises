25f59be3fb7004fe4c03d09f8f9d17b4628e6f76
Alex Bosa <adbosa@gmail.com>
2022-05-07 20:22:32 -0300

    Construction of the exceptions - needs to be fixed.

--- ciscoPythonEssentials/Intermediate/Module4/Lab2_EvaluatingStudentsResults/student_report.py
+++ ciscoPythonEssentials/Intermediate/Module4/Lab2_EvaluatingStudentsResults/student_report.py
@@ -9,33 +9,74 @@ Write a program which:
     Anna Boleyn 	 15.5
     John Smith 	 7.0
 '''
+from os import strerror
+
+class StudentsDataException(Exception):
+    pass
+
+class BadLine(StudentsDataException):
+    def __init__(self, line_number, line_string):
+        StudentsDataException.__init__(self)
+        self.line_number = line_number
+        self.line_string = line_string
+
+class FileEmpty(StudentsDataException):
+    def __init__(self):
+        super().__init__(self)
+
 def main():
     filename = input('Enter the name of the file with student grades: ')
-    return clean_notes(filename).sort()
+    raw_students_grade = extract_data(filename)
+    final_students_grade = clean_notes(raw_students_grade)
+    return final_students_grade
 
-def clean_notes(filename):
-    stream = open(filename, 'rt')
-    notes = {}
-    for line in stream:
+def extract_data(filename):
+    raw_notes = []
+    try:
+        stream = open(filename, 'rt')
+        lines_notes = stream.read().splitlines()
+        for line in lines_notes:
+            raw_notes.append(line.split('\t'))
+        stream.close()
+        if len(raw_notes) < 1:
+            raise FileEmpty
+
+    except IOError as e:
+        print('I/O error occurred: ' + strerror(e.errno))
+    except EmptyFile:
+        print('Source file empty.')
+    
+    return raw_notes
+
+def clean_notes(raw_notes):
+    clean_notes = {}
+    for line in range(len(raw_notes)):
         name = ''
         grade = ''
-        name_length = len(line.split()) - 1
+        name_length = len(raw_notes[line]) - 1
         for item in range(name_length + 1):
             if item < name_length:
-                name += line.split()[item] + ' '
+                name += raw_notes[line][item] + ' '
             else:
-                grade = float(line.split()[item])
+                try:
+                    grade = float(raw_notes[line][item])
+                except ValueError:
+                    raise BadLine(line + 1, raw_notes[line][item])
+                except BadLine as e:
+                    print('Bad line #' + str(e.line_number) + ' in source file:' + line_string)
         name = name.rstrip()
-        if name in notes:
-            notes[name] += grade
+        if name in clean_notes:
+            clean_notes[name] += grade
         else:
-            notes[name] = grade
-    stream.close()
-    return notes
+            clean_notes[name] = grade
+    return clean_notes
 
 def print_grade(grade):
     for key, value in sorted(grade.items()):
         print(f'{key}\t {value}')
 
 if __name__ == '__main__':
-    print_grade(clean_notes('my_notes'))
+    # print(extract_data('my_notes'))
+    # print(clean_notes(extract_data('my_notes')))
+    print_grade(clean_notes(extract_data('my_notes')))
+
