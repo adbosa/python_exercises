f6d99bc9f7785d7b09ec905bde4c93ae89844368
Alex Bosa <adbosa@gmail.com>
2022-05-13 19:26:20 -0300

    Date and time formatting exercise.

---
+++ ciscoPythonEssentials/Intermediate/Module4/Lab4_datetime/datetime_format.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+from datetime import datetime
+
+dt = datetime(2020, 11, 4, 14, 53 )
+print(dt)
+
+## Must show:
+#2020/11/04 14:53:00
+#20/November/04 14:53:00 PM
+#Wed, 2020 Nov 04
+#Wednesday, 2020 November 04
+#Weekday: 3
+#Day of the year: 309
+#Week number of the year: 44
