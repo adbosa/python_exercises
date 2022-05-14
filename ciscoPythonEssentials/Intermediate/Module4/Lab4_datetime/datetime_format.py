d8570bb4785c7b683438cdf1f3344b8a1f828975
Alex Bosa <adbosa@gmail.com>
2022-05-14 14:04:11 -0300

    End of exercise.

--- ciscoPythonEssentials/Intermediate/Module4/Lab4_datetime/datetime_format.py
+++ ciscoPythonEssentials/Intermediate/Module4/Lab4_datetime/datetime_format.py
@@ -4,11 +4,11 @@ from datetime import datetime
 dt = datetime(2020, 11, 4, 14, 53 )
 print(dt)
 
-## Must show:
-#2020/11/04 14:53:00
-#20/November/04 14:53:00 PM
-#Wed, 2020 Nov 04
-#Wednesday, 2020 November 04
-#Weekday: 3
-#Day of the year: 309
-#Week number of the year: 44
+## Command ==> Must show:
+print(dt.strftime('%Y/%m/%d %H:%M:%S')) # 2020/11/04 14:53:00
+print(dt.strftime('%y/%B/%d %H:%M:%S %p')) # 20/November/04 14:53:00 PM
+print(dt.strftime('%a, %Y %b %d'))# Wed, 2020 Nov 04
+print(dt.strftime('%A, %Y %B %d')) # Wednesday, 2020 November 04
+print('Weekday:', dt.strftime('%w')) # Weekday: 3
+print('Day of the year:', dt.strftime('%j'))  # Day of the year: 309
+print('Week number of the year:', dt.strftime('%U')) # Week number of the year: 44
