daa8bf28fb47a9d0e2be3dbe00b8c902fc018b0c
Alex Bosa <adbosa@gmail.com>
2022-05-22 21:25:37 -0300

    Final implementation of the weekdays counter.

--- ciscoPythonEssentials/Intermediate/Module4/Lab5_calendar/weekdays.py
+++ ciscoPythonEssentials/Intermediate/Module4/Lab5_calendar/weekdays.py
@@ -5,9 +5,15 @@ class MyCalendar(Calendar):
     def count_weekday_in_year(self, year, weekday):
         self.year = year
         self.weekday = weekday
+        counter = 0
         for month in range(1, 13):
-            print(self.monthdays2calendar(self.year, month))
+            for week in self.monthdays2calendar(self.year, month):
+                for day, weekday in week:
+                    if day != 0 and weekday == self.weekday:
+                        counter += 1
+        return counter
 
 if __name__ == '__main__':
     first_test = MyCalendar()
-    first_test.count_weekday_in_year(2019, 0)
+    print(first_test.count_weekday_in_year(2019, 0)) # Must be 52
+    print(first_test.count_weekday_in_year(2000, 6)) # Must be 53
